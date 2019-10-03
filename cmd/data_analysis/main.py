68e7ebb6f27985b874d3161549c1ab147d15c39d
Deanna Green <deanna@paddle.com>
2019-10-03 10:40:05 +0100

    first branch commit

--- cmd/data_analysis/main.py
+++ cmd/data_analysis/main.py
@@ -1,13 +1,34 @@
 
 def analyse(words):
 
+    consumer_loop(words)
+    # prosumer_loop(words)
+    # self_serve_loop(words)
+    # sales_assisted_loop(words)
+
+
+def consumer_loop(words):
+
+    consumer_keywords = ['html', 'list', 'consumer']
+    counter = 0
+
+    for i in words:
+        if i in consumer_keywords:
+            counter += 1
+
+    print(counter)
+
+
+
+
+
     # v1
-    return {
-        "consumer_score": 0,
-        "prosumer_score": 1,
-        "self_serve_score": 2,
-        "sales_assisted_score": 3,
-    }
+    # return {
+    #     "consumer_score": 0,
+    #     "prosumer_score": 1,
+    #     "self_serve_score": 2,
+    #     "sales_assisted_score": 3,
+    # }
 
     # v2
     return {
--- cmd/data_collection/main.py
+++ cmd/data_collection/main.py
@@ -4,5 +4,5 @@ from urllib.request import urlopen
 def collect(url):
     #html = urlopen(url).read()
 
-    return ['some', 'words', 'from', 'the', 'html']
+    return ['some', 'words', 'from', 'the', 'html', 'html']
 
