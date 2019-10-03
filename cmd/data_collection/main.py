8a4d4076b988363858481d2d4bf6c0255b770f8c
Beata Lipska <beata@paddle.com>
2019-10-03 12:24:04 +0100

    data collection returns long string

--- cmd/data_collection/main.py
+++ cmd/data_collection/main.py
@@ -1,8 +1,21 @@
 from urllib.request import urlopen
-
+from bs4 import BeautifulSoup
+import string
+import re
 
 def collect(url):
-    #html = urlopen(url).read()
+    html = urlopen(url).read().decode("utf-8")
 
-    return ['some', 'words', 'from', 'the', 'html']
+    sibling_soup = BeautifulSoup(html)
+    for script in sibling_soup(["script", "style"]):
+        script.extract()
+    text = sibling_soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    return text
 
