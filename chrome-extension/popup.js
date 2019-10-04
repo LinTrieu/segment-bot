543481da29ede5e0e492c752665b8431a117091c
Deanna Green <deanna@paddle.com>
2019-10-04 09:43:47 +0100

    added styling and loading gif

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -3,14 +3,16 @@ jQuery(document).ready(function() {
         api_url = "https://gp6j1kfg4g.execute-api.us-east-1.amazonaws.com/default/segment-bot?url=" + tab.url;
         jQuery.get(api_url, function (data) {
             console.log(data);
-            jQuery('#list').append( "<li> Consumer: " + data['consumer']['score'] + "</li>" );
-            jQuery('#list').append( "<p>" + data['consumer']['words'] + "</p>" );
-            jQuery('#list').append( "<li> Prosumer: " + data['prosumer']['score'] + "</li>" );
-            jQuery('#list').append( "<p>" + data['prosumer']['words'] + "</p>" );
-            jQuery('#list').append( "<li> Self Serve Business: " + data['self-serve business']['score'] + "</li>" );
-            jQuery('#list').append( "<p>" + data['self-serve business']['words'] + "</p>" );
-            jQuery('#list').append( "<li> Sales Assisted Business: " + data['sales-assisted business']['score'] + "</li>" );
-            jQuery('#list').append( "<p>" + data['sales-assisted business']['words'] + "</p>" );
+            jQuery('#consumer').append( "<td style=\"width:30.5%\"> Consumer Score: " + data['consumer']['score'] + "</td>" );
+            jQuery('#consumer').append( "<td>" + data['consumer']['words'] + "</td>" );
+            jQuery('#prosumer').append( "<td> Prosumer Score: " + data['prosumer']['score'] + "</td>" );
+            jQuery('#prosumer').append( "<td>" + data['prosumer']['words'] + "</td>" );
+            jQuery('#self-serve').append( "<td> Self Serve Business Score: " + data['self-serve business']['score'] + "</td>" );
+            jQuery('#self-serve').append( "<td>" + data['self-serve business']['words'] + "</td>" );
+            jQuery('#sales-assisted').append( "<td> Sales Assisted Business Score: " + data['sales-assisted business']['score'] + "</td>" );
+            jQuery('#sales-assisted').append( "<td>" + data['sales-assisted business']['words'] + "</td>" );
+            document.getElementById("robot").style.display = "none";
+            document.getElementById("content").style.display = "block";
         })
     })
 });
