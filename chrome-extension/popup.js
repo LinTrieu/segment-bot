3ee7e41edaa15434ca1c33923ecdf112bbd42dca
James Cooke <jcooke@paddle.com>
2019-10-04 13:19:58 +0100

    fixing design issue

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -4,16 +4,16 @@ jQuery(document).ready(function() {
         jQuery.get(api_url, function (data) {
             console.log(data);
             jQuery('#consumer').data('score', data['consumer']['score'])
-            jQuery('#consumer').append( "<td style=\"width:30.5%\"> <b>Consumer</b><br /> Score: " + data['consumer']['score'] + "</td>" );
+            jQuery('#consumer').append( "<td style=\"width:39%\"> <b>Consumer</b><br /> Score: " + data['consumer']['score'] + "</td>" );
             jQuery('#consumer').append( "<td class='words'>" + data['consumer']['words'].join(', ') + "</td>" );
             jQuery('#prosumer').data('score', data['prosumer']['score'])
-            jQuery('#prosumer').append( "<td> <b>Prosumer</b><br /> Score: " + data['prosumer']['score'] + "</td>" );
+            jQuery('#prosumer').append( "<td style=\"width:39%\"> <b>Prosumer</b><br /> Score: " + data['prosumer']['score'] + "</td>" );
             jQuery('#prosumer').append( "<td class='words'>" + data['prosumer']['words'].join(', ') + "</td>" );
             jQuery('#self-serve').data('score', data['self-serve business']['score'])
-            jQuery('#self-serve').append( "<td> <b>Self Serve Business</b><br /> Score: " + data['self-serve business']['score'] + "</td>" );
+            jQuery('#self-serve').append( "<td style=\"width:39%\"> <b>Self Serve Business</b><br /> Score: " + data['self-serve business']['score'] + "</td>" );
             jQuery('#self-serve').append( "<td class='words'>" + data['self-serve business']['words'].join(', ') + "</td>" );
             jQuery('#sales-assisted').data('score', data['sales-assisted business']['score'])
-            jQuery('#sales-assisted').append( "<td> <b>Sales Assisted Business</b><br /> Score: " + data['sales-assisted business']['score'] + "</td>" );
+            jQuery('#sales-assisted').append( "<td style=\"width:39%\"> <b>Sales Assisted Business</b><br /> Score: " + data['sales-assisted business']['score'] + "</td>" );
             jQuery('#sales-assisted').append( "<td class='words'>" + data['sales-assisted business']['words'].join(', ') + "</td>" );
             $("#data-table-body").each(function () {
                 $(this).html($(this).children('tr').sort(function (a, b) {
